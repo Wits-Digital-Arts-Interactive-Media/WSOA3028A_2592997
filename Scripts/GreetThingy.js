5001cc33fc3691592d78dbbc1ce4c3e429d10a0c
Amro <2592997@students.wits.ac.za>
2024-05-10 07:46:27 +0200

    Mhmm

--- Scripts/GreetThingy.js
+++ Scripts/GreetThingy.js
@@ -2,5 +2,5 @@ function displayGreeting()
 {
     var name = document.getElementById("nameInput").value;
     var greeting = document.getElementById("greeting");
-    greeting.textContent = "XSE " + name + ", this is Amro. Welcome to my website. If you didn't bring food go away";
+    greeting.textContent = "Hey " + name + ", this is Amro. Welcome to my website. If you didn't bring food go away";
 }
