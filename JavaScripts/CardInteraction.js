4649ebb2ca4fc7e473b46ddd281c0e8e135730ad
Amro <2592997@students.wits.ac.za>
2024-05-24 14:02:11 +0200

    Update

--- JavaScripts/CardInteraction.js
+++ JavaScripts/CardInteraction.js
@@ -6,31 +6,6 @@ for(let f = 0; f < cards.length; f++){
 }
 
 
-
-
-for(let i = 0; i < cards.length; i++){
-    if (cards[i].addEventListener(mouseover = function(){
-        makeBigger(i);
-        for(let j = 0; j < cards.length; j++){
-            if (j == i){
-                continue;
-            }
-            else{
-                makeSmaller(j);
-            }
-        } 
-    }));
-}
-
-function makeBigger(activeCard){
-    activeCard.style.height = "105%";
-    console.log("its on");
-}
-
-function makeSmaller(inactiveCard){
-    inactiveCard.style.height = "95%";
-}
-
 function clickedOnCard(clickedCard){
     console.log("Clicked on");
 }
