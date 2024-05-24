3b447653adb7a453161cb4e36ce8f65bc4a7382d
Amro <2592997@students.wits.ac.za>
2024-05-24 09:30:49 +0200

    lets cav

--- JavaScripts/Cards.js
+++ JavaScripts/Cards.js
@@ -1,8 +1,8 @@
 const cardClass = 'cardClass';
 const cards = document.getElementsByClassName(cardClass);
 
-for(let i = 0; cards.length; i++){
-    if (cards(i).onmouseover = function(){
+for(let i = 0; i < cards.length; i++){
+    if (cards[i].onmouseover = function(){
         MakeBigger(i)
     });
 }
