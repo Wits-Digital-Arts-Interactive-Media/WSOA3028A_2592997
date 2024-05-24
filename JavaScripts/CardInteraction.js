6a1b14deed5b2b526ae5ef7ce3516f04f1cb74eb
Amro <2592997@students.wits.ac.za>
2024-05-24 23:15:09 +0200

    Its clickable

--- JavaScripts/CardInteraction.js
+++ JavaScripts/CardInteraction.js
@@ -1,12 +1,12 @@
 const cardClass = 'cardClass';
 const cards = document.getElementsByClassName(cardClass);
 
-for(let f = 0; f < cards.length; f++){
-    f.addEventListener("click", clickedOnCard[f]);
+
+
+for (let f = 0; f < cards.length; f++) {
+    cards[f].addEventListener("click", clickedOnCard);
 }
 
-
-function clickedOnCard(clickedCard){
+function clickedOnCard(event) {
     console.log("Clicked on");
 }
-
