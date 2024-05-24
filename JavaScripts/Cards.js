f1275db1bca039679a1f31eb6af68dfc07675696
Amro <2592997@students.wits.ac.za>
2024-05-24 10:40:00 +0200

    Update for this thing

--- JavaScripts/Cards.js
+++ JavaScripts/Cards.js
@@ -1,6 +1,13 @@
 const cardClass = 'cardClass';
 const cards = document.getElementsByClassName(cardClass);
 
+for(let f = 0; f < cards.length; f++){
+    f.addEventListener("click", clickedOnCard[f]);
+}
+
+
+
+
 for(let i = 0; i < cards.length; i++){
     if (cards[i].addEventListener(mouseover = function(){
         makeBigger(i);
@@ -22,5 +29,9 @@ function makeBigger(activeCard){
 
 function makeSmaller(inactiveCard){
     inactiveCard.style.height = "95%";
+}
+
+function clickedOnCard(clickedCard){
+    console.log("Clicked on");
 }
 
