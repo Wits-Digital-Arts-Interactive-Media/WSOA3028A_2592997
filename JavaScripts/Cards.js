f4977890d64a793ee3d9d27ac5756b8caadc701c
Amro <2592997@students.wits.ac.za>
2024-05-24 10:04:35 +0200

    Update

--- JavaScripts/Cards.js
+++ JavaScripts/Cards.js
@@ -2,12 +2,25 @@ const cardClass = 'cardClass';
 const cards = document.getElementsByClassName(cardClass);
 
 for(let i = 0; i < cards.length; i++){
-    if (cards[i].onmouseover = function(){
-        MakeBigger(i)
-    });
+    if (cards[i].addEventListener(mouseover = function(){
+        makeBigger(i);
+        for(let j = 0; j < cards.length; j++){
+            if (j == i){
+                continue;
+            }
+            else{
+                makeSmaller(j);
+            }
+        } 
+    }));
 }
 
-function MakeBigger(ActiveCard){
-    ActiveCard.style.height = "32px";
+function makeBigger(activeCard){
+    activeCard.style.height = "32px";
     console.log("its on");
 }
+
+function makeSmaller(inactiveCard){
+    inactiveCard.style.height = "16px";
+}
+
