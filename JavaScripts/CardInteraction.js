bc4cd33ade45c4037a89a8c5072b79c55cc5e489
Amro <2592997@students.wits.ac.za>
2024-05-24 23:23:33 +0200

    Yah

--- JavaScripts/CardInteraction.js
+++ JavaScripts/CardInteraction.js
@@ -1,12 +1,16 @@
-const cardClass = 'cardClass';
-const cards = document.getElementsByClassName(cardClass);
+document.addEventListener("DOMContentLoaded", function() {
+    const cardClass = 'cardClass';
+    const cards = document.getElementsByClassName(cardClass);
 
+    for (let f = 0; f < cards.length; f++) {
+        cards[f].addEventListener("click", clickedOnCard);
+    }
 
-
-for (let f = 0; f < cards.length; f++) {
-    cards[f].addEventListener("click", clickedOnCard);
-}
-
-function clickedOnCard(event) {
-    console.log("Clicked on");
-}
+    function clickedOnCard(event) {
+        const clickedCard = event.currentTarget;
+        const url = clickedCard.getAttribute('data-url');
+        if (url) {
+            window.location.href = url;
+        }
+    }
+}); 
