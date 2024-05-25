75ebf8b56ec8c6eab0526db184453fc199169364
Amro <2592997@students.wits.ac.za>
2024-05-25 13:24:49 +0200

    I might be Gordon Ramsey

--- JavaScripts/blogTextChange.js
+++ JavaScripts/blogTextChange.js
@@ -1,15 +1,23 @@
-const Blog1 = document.getElementById("week1Text");
+const blogTexts = document.getElementsByClassName("changableText");
+const toTexts = ["Imagining the Internet","Interaction and the www","Interaction Design for the Web","The IxD Process / Headers and Microformats","The Ethics of UI & UX Practice","Internet, Society and Design Justice"," The World in www","Digital Inequalities"];
+const backTexts = ["Week 1","Week 2","Week 3","Week 4","Week 6","Week 8","Week 9","Week 10"];
 
-Blog1.addEventListener("mouseover", changeText);
-Blog1.addEventListener("mouseleave", revertText);
+for(let i= 0; i < blogTexts.length; i++){
+    blogTexts[i].setAttribute('data-index', i);
+    blogTexts[i].addEventListener("mouseover", changeText);
+    blogTexts[i].addEventListener("mouseleave", revertText);
+}
 
 
+console.log(blogTexts);
 function changeText(event) {
     const blogBox = event.currentTarget;
-    const previousText = blogBox.textContent;
-    blogBox.textContent ="It should work now";
+    const index = blogBox.getAttribute('data-index');
+    blogBox.textContent = toTexts[index];
 }
 
 function revertText(event){
-    blogBox.textContent = previousText;
+    const blogBox = event.currentTarget;
+    const index = blogBox.getAttribute('data-index');
+    blogBox.textContent = backTexts[index];
 }
