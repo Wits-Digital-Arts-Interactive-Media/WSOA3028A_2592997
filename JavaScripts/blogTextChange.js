4883ddf29a4b07b5095fa0cfa33886f521fcdb38
Amro <2592997@students.wits.ac.za>
2024-05-25 13:49:49 +0200

    Sho

--- JavaScripts/blogTextChange.js
+++ JavaScripts/blogTextChange.js
@@ -2,18 +2,17 @@ const blogTexts = document.getElementsByClassName("changableText");
 const toTexts = ["Imagining the Internet","Interaction and the www","Interaction Design for the Web","The IxD Process / Headers and Microformats","The Ethics of UI & UX Practice","Internet, Society and Design Justice"," The World in www","Digital Inequalities"];
 const backTexts = ["Week 1","Week 2","Week 3","Week 4","Week 6","Week 8","Week 9","Week 10"];
 
+
 for(let i= 0; i < blogTexts.length; i++){
     blogTexts[i].setAttribute('data-index', i);
     blogTexts[i].addEventListener("mouseover", changeText);
     blogTexts[i].addEventListener("mouseleave", revertText);
 }
 
-
-console.log(blogTexts);
 function changeText(event) {
     const blogBox = event.currentTarget;
     const index = blogBox.getAttribute('data-index');
-    blogBox.textContent = toTexts[index];
+    blogBox.textContent = toTexts[index];    
 }
 
 function revertText(event){
