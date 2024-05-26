5ffb0599a23a927f6a7885f8a501706da89b1ced
Amro <2592997@students.wits.ac.za>
2024-05-26 16:53:09 +0200

    Sho

--- JavaScripts/introPage.js
+++ JavaScripts/introPage.js
@@ -1,8 +1,10 @@
+window.onload = function() {
+    localStorage.setItem('visited', 'false'); // Reset 'visited' flag to 'false' on every page load
+};
+
 const Page = document.getElementById("startupPage");
 
 console.log("localStorage.getItem('visited'):", localStorage.getItem('visited'));
-
-
 
 if (localStorage.getItem('visited') !== 'true') {
     console.log("its false");
