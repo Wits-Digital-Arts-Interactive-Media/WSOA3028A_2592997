3e43930ad5ba5dd2276bbc85cd0ce78f64404c5b
Amro <2592997@students.wits.ac.za>
2024-06-20 14:17:35 +0200

    Revert "dark theme"
    This reverts commit 69f004d4a9c29cd186e3c49c8e9afd60e81c7369.

--- JavaScripts/footerBar.js
+++ JavaScripts/footerBar.js
@@ -4,21 +4,14 @@ const options = [
     //{name : "hide", href:`/WSOA3028A_2592997/index.html`}
 ];
 
-
 const selectedElements = [
     {element: document.body},
     {element: document.querySelector('#mainHeader')},
 ];
 
 var darkModeActive = false; 
-
 const elements = document.getElementsByClassName("cardClass");
 const cards = Array.from(elements);
-
-const cardElements = document.getElementsByClassName("selectable");
-const blogCards = Array.from(cardElements);
-
-
 
 export function createFooter(){
     const nav = document.querySelector("body > footer > nav");
@@ -47,10 +40,6 @@ export function createFooter(){
         }
     });
 
-    if (localStorage.getItem('darkModeOn') == 'true'){
-        darkMode();
-    }
-
     const hideButtonLi = document.createElement("li");
     const hideButton = document.createElement("button");
     hideButton.id = "hideContainer";
@@ -74,7 +63,6 @@ export function createFooter(){
 }
 
 function darkMode(){
-    localStorage.setItem('darkModeOn', 'true');
     document.body.style.backgroundColor = 'rgb(58, 58, 58)';
     darkModeActive = true;
     for (let i = 0; i < selectedElements.length; i++){
@@ -83,10 +71,6 @@ function darkMode(){
     
     for (let f = 0; f < cards.length; f++){
         cards[f].style.backgroundColor = 'rgb(99, 99, 99)';
-    }
-
-    for (let g = 0; g < blogCards.length; g++){
-        blogCards[g].style.backgroundColor = 'rgb(99, 99, 99)';
     }
 
     const footer = document.getElementById("footerBar");
@@ -110,10 +94,6 @@ function lightMode(){
         cards[f].style.backgroundColor = 'rgb(228, 228, 228)';
     }
 
-    for (let g = 0; g < blogCards.length; g++){
-        blogCards[g].style.backgroundColor = 'rgb(228, 228, 228)';
-    }
-
     const footer = document.getElementById("footerBar");
     footer.style.backgroundColor = 'rgb(228, 228, 228)';
 
@@ -122,20 +102,4 @@ function lightMode(){
 
     const darkModeButton = document.getElementById("toggleSwitch");
     darkModeButton.style.backgroundColor = 'rgb(243, 243, 243)';
-    
 }
-
-let disableBeforeUnload = false;
-
-document.addEventListener('click', function(event) {
-    const targetElement = event.target.closest('a');
-    if (targetElement && targetElement.href.startsWith(window.location.origin)) {
-        disableBeforeUnload = true;
-    }
-});
-
-window.addEventListener('beforeunload', function(event) {
-    if (!disableBeforeUnload) {
-        localStorage.setItem('darkModeOn', 'false');
-    }
-});
